32072ee54a838801b72c5c0c9ce4cd55edfbd3ef
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-01-04 15:37:45 +0600

    Create empty project

---
+++ src/main/java/kg/bilingual/bilingual/BilingualApplication.java
@@ -0,0 +1,14 @@
+package kg.bilingual.bilingual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingualApplication.class, args);
+		System.out.println("Hey Aliaskar, Atabek, Aizhan, RaimMillion, the project name is Bilingual");
+	}
+
+}
---
+++ src/test/java/kg/bilingual/bilingual/BilingualApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bilingual.bilingual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
